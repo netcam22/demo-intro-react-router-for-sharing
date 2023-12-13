591c4bafa86c0ecd965033f61519c22ef53b8d7e
netcam22 <annettelesage@btinternet.com>
2023-12-13 12:34:42 +0000

    feat: complete class task

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,16 @@
-export {}
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+    return (
+    <>
+    <Header/>
+     <main className='p-6' style={{flex:'1'}}>
+        <Outlet/>
+     </main>
+    <Footer/>
+    </>
+    );
+}
+export default MainLayout;
